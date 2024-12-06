219209a47584cd69c4eadddc6e36ed5fe0a0229b
LuisRiofrio <lriofrio915@gmail.com>
2024-12-06 11:36:54 -0500

    se actualiza la pagina de favoritos

--- src/Routes/Favs.jsx
+++ src/Routes/Favs.jsx
@@ -1,18 +1,30 @@
-import React from "react";
+import React, { useEffect, useState, useContext } from "react";
 import Card from "../Components/Card";
-
-//Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
+import { ContextGlobal } from "../Components/utils/global.context";
 
 const Favs = () => {
+  const [favorites, setFavorites] = useState([]);
+  const { theme } = useContext(ContextGlobal);
+
+  useEffect(() => {
+    const storedFavorites = JSON.parse(localStorage.getItem("favorites")) || [];
+    setFavorites(storedFavorites);
+  }, []);
 
   return (
-    <>
+    <div className={theme}>
+      {" "}
       <h1>Dentists Favs</h1>
       <div className="card-grid">
-        {/* este componente debe consumir los destacados del localStorage */}
-        {/* Deberan renderizar una Card por cada uno de ellos */}
+        {favorites.length > 0 ? (
+          favorites.map((dentist) => (
+            <Card key={dentist.id} dentist={dentist} />
+          ))
+        ) : (
+          <p>No hay dentistas destacados.</p>
+        )}
       </div>
-    </>
+    </div>
   );
 };
 
